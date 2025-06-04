7829151dd015ba8084b4a92467a27052a886d26b
adam jaidi <163515896+AdamJaidiEhb@users.noreply.github.com>
2025-06-04 15:30:14 +0200

    test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Welkom op Home</Text>
+      <Text> Welkom op Home</Text>
     </View>
   );
 }
